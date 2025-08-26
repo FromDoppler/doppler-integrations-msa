e1fb49627f7d42cce00f4c3c78fc331e2387d1e9
emoralesms <emorales@makingsense.com>
2025-08-26 17:22:30 -0300

    feat: rfm model

---
+++ doppler-integrations-domain/RfmModel.cs
@@ -0,0 +1,10 @@
+namespace DopplerIntegrationsDomain
+{
+    public class RfmModel
+    {
+        public bool Visible { get; set; }
+        public bool Active { get; set; }
+        public string Period { get; set; }
+        public string Date { get; set; }
+    }
+}
