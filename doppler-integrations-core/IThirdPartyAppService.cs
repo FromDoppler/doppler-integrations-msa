7b4eaffb67fa1d13e3581482704565038be394c8
emoralesms <emorales@makingsense.com>
2025-08-26 17:22:47 -0300

    feat: interfaces

--- doppler-integrations-core/IThirdPartyAppService.cs
+++ doppler-integrations-core/IThirdPartyAppService.cs
@@ -5,5 +5,9 @@ namespace DopplerIntegrationsCore
     public interface IThirdPartyAppService
     {
         Task<IList<ThirdPartyAppXUser>> GetListThirdPartyAppByUser(int idUser);
+
+        Task<ThirdPartyAppXUser> GetThirdPartyAppXUser(int idThirdPartyApp, int idUser);
+
+        Task<RfmModel> GetRfmModel(ThirdPartyAppXUser dto);
     }
 }
--- doppler-integrations-data/IThirdPartyAppXUserRepository.cs
+++ doppler-integrations-data/IThirdPartyAppXUserRepository.cs
@@ -5,5 +5,7 @@ namespace DopplerIntegrationsData
     public interface IThirdPartyAppXUserRepository
     {
         Task<IList<ThirdPartyAppXUser>> GetListThirdPartyAppByUser(int idUser);
+        Task<ThirdPartyAppXUser> GetThirdPartyAppXUser(int idUser, int idThirdPartyApp);
+        Task<ThirdPartyApp> GetThirdPartyAppById(int idThirdPartyApp);
     }
 }
