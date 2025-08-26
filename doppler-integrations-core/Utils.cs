feefcff8f8527b9cfdff4f88a2db9210aa8cd3c8
emoralesms <emorales@makingsense.com>
2025-08-26 17:22:18 -0300

    feat: utils

---
+++ doppler-integrations-core/Utils.cs
@@ -0,0 +1,16 @@
+using System.Globalization;
+
+namespace DopplerIntegrationsCore
+{
+    public class Utils
+    {
+        public static string FormatDateTimeAsString(DateTime? date, string culture = "")
+        {
+            return date == null
+                ? ""
+                : culture == "es"
+                ? string.Format(new CultureInfo("es-ES"), "{0:dd/MM/yyyy hh:mm tt}", date)
+                : string.Format(CultureInfo.InvariantCulture, "{0:MM/dd/yyyy hh:mm tt}", date);
+        }
+    }
+}
