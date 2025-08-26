760b35df2e4c7306121489ef456f7b88e78a5634
emoralesms <emorales@makingsense.com>
2025-08-26 17:23:10 -0300

    feat: service and repository

--- doppler-integrations-core/ThirdPartyAppService.cs
+++ doppler-integrations-core/ThirdPartyAppService.cs
@@ -1,4 +1,4 @@
-using System.Runtime;
+using System.Globalization;
 using DopplerIntegrationsData;
 using DopplerIntegrationsDomain;
 
@@ -18,6 +18,42 @@ namespace DopplerIntegrationsCore
         public async Task<IList<ThirdPartyAppXUser>> GetListThirdPartyAppByUser(int idUser)
         {
             return await _repository.GetListThirdPartyAppByUser(idUser);
+        }
+
+        public async Task<ThirdPartyAppXUser> GetThirdPartyAppXUser(int idThirdPartyApp, int idUser)
+        {
+            var appXUser = await _repository.GetThirdPartyAppXUser(idUser, idThirdPartyApp);
+            if (appXUser != null)
+            {
+                //int offset = 0;
+                //UserTimeZone timeZone = _timeZoneRepository.GetByUserId(idUser);
+                //if (timeZone != null)
+                //{
+                //    offset = timeZone.Offset;
+                //}
+
+                //appXUser.UTCLastUpdate = appXUser.UTCLastUpdate.AddMinutes(offset);
+            }
+
+            return appXUser;
+        }
+
+        public async Task<RfmModel> GetRfmModel(ThirdPartyAppXUser dto)
+        {
+            if (dto != null)
+            {
+                var thirdPartyApp = await _repository.GetThirdPartyAppById(dto.IdThirdPartyApp);
+
+                return new RfmModel()
+                {
+                    Visible = thirdPartyApp.RFMEnabled,
+                    Active = dto.RFMActive,
+                    Period = dto.RFMPeriod.ToString(CultureInfo.InvariantCulture),
+                    Date = Utils.FormatDateTimeAsString(dto.UTCLastRFMCalc) ?? string.Empty
+                };
+            }
+
+            return null;
         }
     }
 }
--- doppler-integrations-data/ThirdPartyAppXUserRepository.cs
+++ doppler-integrations-data/ThirdPartyAppXUserRepository.cs
@@ -1,3 +1,4 @@
+using System.Data.SqlClient;
 using DopplerIntegrationsData.Helpers;
 using DopplerIntegrationsDomain;
 using Microsoft.Extensions.Options;
@@ -66,6 +67,127 @@ namespace DopplerIntegrationsData
             }
 
             return Task.FromResult(list);
+        }
+
+        public Task<ThirdPartyAppXUser> GetThirdPartyAppXUser(int idUser, int idThirdPartyApp)
+        {
+            ThirdPartyAppXUser result = null;
+
+            var query = @"
+                SELECT TOP 1
+                    [IdUser],
+                    [IdThirdPartyApp],
+                    [AccessToken],
+                    [RefreshToken],
+                    [IdAccount],
+                    [AccountName],
+                    [UTCLastUpdate],
+                    [SendNotificationEmail],
+                    [UTCLastCompletedSync],
+                    [SourceType],
+                    [ConnectionErrors],
+                    [UTCLastValidation],
+                    [UTCCreationDate],
+                    [RFMActive],
+                    [RFMPeriod],
+                    [UTCLastRFMCalc],
+                    [BQUpdateDate],
+                    [UTCTokenExpiration],
+                    [UTCLastAssistedShoppingSync]
+                FROM [Doppler2011].[dbo].[ThirdPartyAppXUser]
+                WHERE IdUser = @idUser AND IdThirdPartyApp = @idThirdPartyApp";
+
+            using (var reader = SQLHelper.ExecuteReader(
+                SQLHelper.CONN_STRING,
+                query,
+                new SqlParameter("@idUser", idUser),
+                new SqlParameter("@idThirdPartyApp", idThirdPartyApp)))
+            {
+                if (reader.Read())
+                {
+                    result = new ThirdPartyAppXUser
+                    {
+                        IdUser = NullTreatment.GetInt(reader.GetSqlValue(0)),
+                        IdThirdPartyApp = NullTreatment.GetInt(reader.GetSqlValue(1)),
+                        AccessToken = NullTreatment.GetString(reader.GetSqlValue(2)),
+                        RefreshToken = NullTreatment.GetString(reader.GetSqlValue(3)),
+                        IdAccount = NullTreatment.GetLong(reader.GetSqlValue(4)),
+                        AccountName = NullTreatment.GetString(reader.GetSqlValue(5)),
+                        UTCLastUpdate = NullTreatment.GetDateTime(reader.GetSqlValue(6)),
+                        SendNotificationEmail = NullTreatment.GetBoolean(reader.GetSqlValue(7)),
+                        UTCLastCompletedSync = NullTreatment.GetDateTime(reader.GetSqlValue(8)),
+                        SourceType = NullTreatment.GetInt(reader.GetSqlValue(9)),
+                        ConnectionErrors = NullTreatment.GetInt(reader.GetSqlValue(10)),
+                        UTCLastValidation = NullTreatment.GetDateTime(reader.GetSqlValue(11)),
+                        UTCCreationDate = NullTreatment.GetDateTime(reader.GetSqlValue(12)),
+                        RFMActive = NullTreatment.GetBoolean(reader.GetSqlValue(13)),
+                        RFMPeriod = NullTreatment.GetInt(reader.GetSqlValue(14)),
+                        UTCLastRFMCalc = NullTreatment.GetDateTime(reader.GetSqlValue(15)),
+                        BQUpdateDate = NullTreatment.GetDateTime(reader.GetSqlValue(16)),
+                        UTCTokenExpiration = NullTreatment.GetDateTime(reader.GetSqlValue(17)),
+                        UTCLastAssistedShoppingSync = NullTreatment.GetDateTime(reader.GetSqlValue(18))
+                    };
+                }
+
+                reader.Close();
+            }
+
+            return Task.FromResult(result);
+        }
+
+        public Task<ThirdPartyApp> GetThirdPartyAppById(int idThirdPartyApp)
+        {
+            ThirdPartyApp app = null;
+
+            var query = @"
+                SELECT TOP 1
+                    [IdThirdPartyApp],
+                    [Name],
+                    [Active],
+                    [ProductsEnabled],
+                    [AbandonedCartEnabled],
+                    [VisitedProductsEnabled],
+                    [CrossSellingEnabled],
+                    [BestSellingEnabled],
+                    [NewProductsEnabled],
+                    [PendingOrderEnabled],
+                    [ConfirmationOrderEnabled],
+                    [RFMEnabled],
+                    [PromotionCodeEnabled],
+                    [AssistedShoppingEnabled]
+                FROM [Doppler2011].[dbo].[ThirdPartyApp]
+                WHERE IdThirdPartyApp = @idThirdPartyApp";
+
+            using (var reader = SQLHelper.ExecuteReader(
+                SQLHelper.CONN_STRING,
+                query,
+                new SqlParameter("@idThirdPartyApp", idThirdPartyApp)))
+            {
+                if (reader.Read())
+                {
+                    app = new ThirdPartyApp
+                    {
+                        IdThirdPartyApp = NullTreatment.GetInt(reader.GetSqlValue(0)),
+                        Name = NullTreatment.GetString(reader.GetSqlValue(1)),
+                        Active = NullTreatment.GetBoolean(reader.GetSqlValue(2)),
+                        ProductsEnabled = NullTreatment.GetBoolean(reader.GetSqlValue(3)),
+                        AbandonedCartEnabled = NullTreatment.GetBoolean(reader.GetSqlValue(4)),
+                        VisitedProductsEnabled = NullTreatment.GetBoolean(reader.GetSqlValue(5)),
+                        CrossSellingEnabled = NullTreatment.GetBoolean(reader.GetSqlValue(6)),
+                        BestSellingEnabled = NullTreatment.GetBoolean(reader.GetSqlValue(7)),
+                        NewProductsEnabled = NullTreatment.GetBoolean(reader.GetSqlValue(8)),
+                        PendingOrderEnabled = NullTreatment.GetBoolean(reader.GetSqlValue(9)),
+                        ConfirmationOrderEnabled = NullTreatment.GetBoolean(reader.GetSqlValue(10)),
+                        RFMEnabled = NullTreatment.GetBoolean(reader.GetSqlValue(11)),
+                        PromotionCodeEnabled = NullTreatment.GetBoolean(reader.GetSqlValue(12)),
+                        AssistedShoppingEnabled = NullTreatment.GetBoolean(reader.GetSqlValue(13))
+                    };
+                }
+
+                reader.Close();
+            }
+
+            return Task.FromResult(app);
         }
     }
 }
