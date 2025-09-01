4b5d800c3adda841452bd89ae6460de519012db4
emoralesms <emorales@makingsense.com>
2025-09-01 10:52:07 -0300

    feat: get current language utils

--- doppler-integrations-core/Utils.cs
+++ doppler-integrations-core/Utils.cs
@@ -12,5 +12,10 @@ namespace DopplerIntegrationsCore
                 ? string.Format(new CultureInfo("es-ES"), "{0:dd/MM/yyyy hh:mm tt}", date)
                 : string.Format(CultureInfo.InvariantCulture, "{0:MM/dd/yyyy hh:mm tt}", date);
         }
+
+        public static string GetCurrentLanguage(int idLanguage)
+        {
+            return idLanguage == 1 ? "es" : "en";
+        }
     }
 }
